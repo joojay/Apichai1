5bfadfb46998080143c3f90ce71499e594f7b25f
apichai limsiriworakul <apichai.4288.ap@gmail.com>
2019-03-17 12:06:32 +0700

    Edit Update

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -6,7 +6,22 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+ /* items = ["Jinhwan",
+          "June",
+          "Hanbin",
+          "Bobby",
+          "Chanwoo"];*/
+  items = [
+      { name: "jinhwan" , tel : "0825520276", avatar :"1.jpg"},
+      { name: "June" , tel : "0825520276", avatar : "2.jpg"},
+      { name: "Hanbin" , tel : "0825520276" , avatar: "3.jpg"},
+      { name: "Hanbin" , tel : "0825520276" , avatar :"3.jpg"},
+      { name: "Bobby", tel :"0825520276", avatar :"4.jpg"}   
+  ];
 
+  itemSelected(item : string){
+    console.log("Selected Item" ,item);
+}
   constructor(public navCtrl: NavController) {
 
   }
